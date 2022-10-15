470f58b6e76e9c8cf7eda67a166e120cdb67dcb8
max1998 <kbthemamba56@gmail.com>
2022-10-15 12:54:51 -0300

    agregando elemento modal en "cultivos"

--- js/tarjetas.js
+++ js/tarjetas.js
@@ -1,35 +1,87 @@
 //----------------- ELEMENTOS DOM  -----------------//
 let d = {
-    botones: document.querySelectorAll("button"),
-    botonesSembrar: document.querySelectorAll(".sembrar"),
-    botonesFav: document.querySelectorAll(".favoritos"),
+    titulos: document.querySelectorAll(".titulos"),
     body: document.querySelector("body"),
+    titulo_seleccionado: null,
     lightbox: null,
-    seleccion: null,
+    modal: null,
+    icono_cerrar: null,
+    img_cerrar: null,
+    header_modal: null,
+    descripcion_modal: null,
+    botones_modal: null,
+    sembrar_modal: null,
+    fav_modal: null,
 }
 
 //----------------- METODOS  -----------------//
 let m = {
-    agregarEventos:function(){
-        d.botones.forEach(element => {
-            element.addEventListener("click", m.identificarClaseBoton);
+    agregandoEventos:function(){
+        d.titulos.forEach(element => {
+            element.addEventListener("click", m.identificarTarjeta);
         });
     },
 
-    identificarClaseBoton:function(boton){
-        d.seleccion = boton.target.className;
-        if (d.seleccion == "sembrar") {
-            alert("el cultivo ha sido sembrado y añadido a 'mi jardin' ");
-            // posteriormente se agrega una alerta tipo modal
-
-        } else if(d.seleccion == "favoritos"){
-            alert("el cultivo se agrego a favoritos en 'mi jardin'");
-            // posteriormente se agrega una alerta tipo modal
-        }
+    identificarTarjeta:function(tarjeta){
+        d.titulo_seleccionado = tarjeta.target.id;
+        console.log(d.titulo_seleccionado);
+        m.crearLightbox();
     },
-    
+
+    crearLightbox:function(){
+        d.body.appendChild(document.createElement("div")).setAttribute("class","lightbox");
+        //los estilos se añaden via CSS
+        d.lightbox = document.querySelector(".lightbox");
+        m.crearModal(d.lightbox);
+    },
+
+    crearModal:function(lightbox){
+        //---- variables para estructura html
+        let cerrar_img = "<img src='../recursos/icono-cerrar.png'>";
+        let isologo = "<img src='../recursos/icono-isologo.png'>";
+        let infoModal = "<h6> INFORMACIÓN </h6>";
+        let botonSembrar = '<button type="button" class="sembrar"> SEMBRAR </button>';
+        let botonFav = '<button type="button" class="favoritos"> A FAVORITOS ★</button>';
+
+        //----se agrega elemento modal-----
+        lightbox.appendChild(document.createElement("div")).setAttribute("id","modal_tarjetas");
+        d.modal = document.querySelector("#modal_tarjetas")
+        //los estilos se añaden via CSS, los cuales varian dependiendo la resolución*/
+        d.modal.classList.add("modal_tarjetas_movil");
+        
+        //------- icono cerrar ------
+        d.modal.appendChild(document.createElement("div")).setAttribute("class", "icono_cerrar");
+        d.icono_cerrar = document.querySelector(".icono_cerrar");
+        d.icono_cerrar.insertAdjacentHTML("afterbegin", cerrar_img);
+        d.img_cerrar = document.querySelector(".icono_cerrar img")
+        d.img_cerrar.addEventListener("click",m.cerrarModal);      
+
+        //-------header modal------
+        d.modal.appendChild(document.createElement("div")).setAttribute("class","header_modal");
+        d.header_modal = document.querySelector(".header_modal");
+        d.header_modal.insertAdjacentHTML("afterbegin", isologo);
+
+        //-------descripcion modal------
+        d.modal.appendChild(document.createElement("div")).setAttribute("class","descripcion_modal");
+        d.descripcion_modal = document.querySelector(".descripcion_modal");
+        d.descripcion_modal.insertAdjacentHTML("afterbegin", infoModal);
+
+        //-------botones modal------
+        d.modal.appendChild(document.createElement("div")).setAttribute("class","botones_modal");
+        d.botones_modal = document.querySelector(".botones_modal");
+        d.botones_modal.insertAdjacentHTML("afterbegin", botonSembrar);
+        d.botones_modal.insertAdjacentHTML("beforeend", botonFav);
+        
+    },
+
+    cerrarModal:function(){
+        d.lightbox.remove();
+    }
+
+
+
 
 
 }
 
-m.agregarEventos();
+m.agregandoEventos();
