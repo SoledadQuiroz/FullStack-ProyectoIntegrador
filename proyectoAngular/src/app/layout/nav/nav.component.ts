d6ce9dff24af315bf73b7c64553ed884f682ca7e
Maximilian <72700320+maxtheprogrammer98@users.noreply.github.com>
2023-04-30 17:28:17 -0300

    vista movil sección tienda terminada

--- proyectoAngular/src/app/layout/nav/nav.component.ts
+++ proyectoAngular/src/app/layout/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
-
 }
