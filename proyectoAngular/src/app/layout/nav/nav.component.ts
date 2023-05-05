c0e92c9a72a2b74be0885a7d2d910c16e62c0746
Maximilian <72700320+maxtheprogrammer98@users.noreply.github.com>
2023-05-05 18:09:06 -0300

    nav arreglado

--- proyectoAngular/src/app/layout/nav/nav.component.ts
+++ proyectoAngular/src/app/layout/nav/nav.component.ts
@@ -1,33 +1,16 @@
-import { Component} from '@angular/core';
+import { Component, HostListener, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
-  // variable para controlar el estado del nav
-  // false = cerrado / open = abierto:
-  //isNavOpen = false;
-  // toma la resolucion actual
-  //resolucion = window.innerWidth;
-  // valida si la res es escritorio
-  //isDesktop = false;
-  
-  // funcion para cambiar el estado del nav:
-  // toggleNav() {
-  //   // al abrirse cambia de estado
-  //   this.isNavOpen = !this.isNavOpen;
-  // }
+export class NavComponent{
+  // variable flag
+  toggleEffect = false;
 
-  // funcion para ajustar los estilos del nav:
-  // estilosNav():string{
-  //   if(this.resolucion <= 600 && this.isNavOpen){
-  //     return "nav_movil";
-  //   } else{
-  //     this.isNavOpen = false;
-  //     return "nav_normal";
-  //   }
-  // }
-
+  // funcion para cambiar el estado de la v. flag
+  desplegarMenu() {
+    this.toggleEffect = !this.toggleEffect;
+  }
 }
