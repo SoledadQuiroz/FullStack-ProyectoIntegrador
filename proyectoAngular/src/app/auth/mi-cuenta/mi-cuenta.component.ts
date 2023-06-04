16966b48bd2568d3448f47d501962a796cf5671c
Soledad Quiroz <soledadquiroz96@gmail.com>
2023-06-04 00:06:40 -0300

    Modificando registro a formulario reactivo

--- proyectoAngular/src/app/auth/mi-cuenta/mi-cuenta.component.ts
+++ proyectoAngular/src/app/auth/mi-cuenta/mi-cuenta.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-mi-cuenta',
   templateUrl: './mi-cuenta.component.html',
   styleUrls: ['./mi-cuenta.component.css']
 })
-export class MiCuentaComponent {
+export class MiCuentaComponent implements OnInit {
+  userForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    birthdate: ['', Validators.required]
+  });
 
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+      this.router.navigateByUrl("");
+      this.userForm.reset();
+    }
+    else {
+      this.userForm.markAllAsTouched();
+    }
+  }
 }
