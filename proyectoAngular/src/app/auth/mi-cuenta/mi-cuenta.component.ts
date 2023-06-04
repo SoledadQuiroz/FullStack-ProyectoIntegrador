ebfb873083c235e8feebaeec2737fba9f6148fc6
Soledad Quiroz <soledadquiroz96@gmail.com>
2023-06-04 18:30:42 -0300

    Validación de contraseña

--- proyectoAngular/src/app/auth/mi-cuenta/mi-cuenta.component.ts
+++ proyectoAngular/src/app/auth/mi-cuenta/mi-cuenta.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-mi-cuenta',
@@ -9,27 +8,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./mi-cuenta.component.css']
 })
 export class MiCuentaComponent implements OnInit {
-  userForm = this.formBuilder.group({
-    username: ['',[ Validators.required,Validators.minLength(4)]],
-    password: ['',[ Validators.required,Validators.minLength(8)]],
-    confirmPassword: ['', Validators.required],
-    name: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    birthdate: ['', Validators.required]
-  });
+  userForm!: FormGroup;
 
   constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.userForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, this.passwordMatchValidator]],
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      birthdate: ['', Validators.required]
+    });
   }
+
+
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl): { [key: string]: boolean } | null => {
+    const password = this.userForm?.get('password')?.value;
+    const confirmPassword = control.value;
+
+    if (password !== confirmPassword) {
+      return { 'passwordMismatch': true };
+    }
+
+    return null;
+  };
 
   onSubmit() {
     if (this.userForm.valid) {
       console.log(this.userForm.value);
-      this.router.navigateByUrl("");
+      this.router.navigateByUrl('');
       this.userForm.reset();
-    }
-    else {
+    } else {
       this.userForm.markAllAsTouched();
     }
   }
