f104b5a731246287774305239b5af0711e8f2495
Soledad Quiroz <soledadquiroz96@gmail.com>
2023-06-14 17:03:23 -0300

    Cerrar sesión terminado.

--- proyectoAngular/src/app/layout/header/header.component.ts
+++ proyectoAngular/src/app/layout/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/Auth/auth.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  estaAutenticado: boolean = false;
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.authService.estaAutenticado.subscribe(res => {
+      this.estaAutenticado = res;
+    });
+  }
+
+  onCerrarSesion() {
+    this.authService.logout();
+    this.estaAutenticado = false;
+    this.router.navigate(['/home']);
+  }
 }
