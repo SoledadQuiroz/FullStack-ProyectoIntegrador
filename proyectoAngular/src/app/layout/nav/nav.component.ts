5c9b7e931d6da03b86e1226c4c96aee184c6b8ec
Soledad Quiroz <soledadquiroz96@gmail.com>
2023-06-13 23:22:11 -0300

    Funcionamiento del nav si está o no autenticado

--- proyectoAngular/src/app/layout/nav/nav.component.ts
+++ proyectoAngular/src/app/layout/nav/nav.component.ts
@@ -1,11 +1,24 @@
-import { Component, HostListener, Renderer2} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/Auth/auth.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent{
+export class NavComponent implements OnInit{
+  [x: string]: any;
+  estaAutenticado:boolean=false;
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.authService.estaAutenticado.subscribe(res=>( this.estaAutenticado=res));
+   }
+
   // variable flag
   toggleEffect = false;
 
